654b2976bbacfd8e924dc56bb30ea14ef4a9b650
AtecStudios <VictoryForPhil@gmail.com>
2017-02-05 21:02:05 -0800

    Generate Files from Template and RUn

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 #!/usr/bin/env node
+const unzip = require('unzip');
 var program = require('commander');
+const fs = require('fs');
+var exec = require('child_process').spawn;
 program
-  .command('create <name>')
-  .description('Create a Disnode Project')
-  .action(function(name){
-    console.log("Creating " + name);
-  });
+	.command('create <name>')
+	.description( 'Create a Disnode Project')
+	.action(function (name) {
+		console.log("Creating " + name);
+		fs.createReadStream('./disnode_template.zip')
+		.pipe(unzip.Extract({ path: name+'/' }))
+		.on("finish",function(){
+			console.log("- Created Template!");
+			var process = exec('npm install',{cwd: './' + name}, function(error, stdout, stderr) {
+
+			});
+
+		});
+	});
 program
-  .command('install <plugin>')
-  .description('Install a Disnode Plugin')
-  .action(function(plugin){
-    console.log("Installing " + plugin);
-  });
+	.command('install <plugin>')
+	.description('Install a Disnode Plugin')
+	.action(function (plugin) {
+		console.log("Installing " + plugin);
+	});
 
-  program
-    .command('uninstall <plugin>')
-    .description('Uninstall a Disnode Plugin')
-    .action(function(plugin){
-      console.log("Uninstalling " + plugin);
-    });
+program
+	.command('uninstall <plugin>')
+	.description('Uninstall a Disnode Plugin')
+	.action(function (plugin) {
+		console.log("Uninstalling " + plugin);
+	});
 
-    program
-      .command('launch')
-      .description('Launch the bot')
-      .action(function(){
-        console.log("RUNNING! " );
-      });
+program
+	.command('launch')
+	.description('Launch the bot')
+	.action(function () {
+		var proc = exec('node Run.js',{stdio: "inherit"}, function(error, stdout, stderr) {
+
+		});
+		proc.on('data', function (data) {
+  	console.log('stdout: ' + data.toString());
+});
+		console.log("RUNNING! ");
+	});
 program.parse(process.argv);
+
+
+function InstallPlugin(name) {
+	if (!name) {
+		console.log("Please Enter a name for a plugin!");
+	}
+}
